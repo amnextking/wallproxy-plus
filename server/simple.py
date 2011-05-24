85d7733fed6a9957fdf4f70b68aebf76cd757983
dtmajia <dtmajia@163.com>
2011-05-24 08:27:33 +0000

    Optimization

--- server/simple.py
+++ server/simple.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2010-2011 | GNU GPLv3
 __author__ = 'ZHRtYWppYUAxNjMuY29t'.decode('base64')
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 import gaeproxy
 
@@ -8,16 +8,11 @@ class MainHandler(gaeproxy.MainHandler):
     _cachename = 'wp_cache2'
 
     def dump_data(self, dic):
-        if 'headers' in dic:
-            dic = dict(dic)
-            dic['headers'] = '&'.join('%s=%s' % (k,str(v).encode('hex')) for k,v in dic['headers'].iteritems())
         return '&'.join('%s=%s' % (k,str(v).encode('hex')) for k,v in dic.iteritems())
 
     def load_data(self, qs):
-        dic = dict((k,v.decode('hex')) for k,v in (x.split('=') for x in qs.split('&')))
-        if 'headers' in dic:
-            dic['headers'] = dict((k,v.decode('hex')) for k,v in (x.split('=') for x in dic['headers'].split('&')))
-        return dic
+        return dict((k,v.decode('hex')) for k,v in (x.split('=') for x in qs.split('&')))
+
 
 def main():
     application = gaeproxy.webapp.WSGIApplication([(r'/.*', MainHandler)], debug=True)
