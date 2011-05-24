d8fcb5904aa72a7d5f188743779fee3f4dcf4f9d
dtmajia <dtmajia@163.com>
2011-05-24 08:29:08 +0000

    Optimization

--- local/src.zip/plugins/simple.py
+++ local/src.zip/plugins/simple.py
@@ -1,21 +1,24 @@
 # Copyright (C) 2010-2011 | GNU GPLv3
 __author__ = 'ZHRtYWppYUAxNjMuY29t'.decode('base64')
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 import gaeproxy
 
 class Handler(gaeproxy.Handler):
     def dump_data(self, dic):
-        if 'headers' in dic:
-            dic = dict(dic)
-            dic['headers'] = '&'.join('%s=%s' % (k,str(v).encode('hex')) for k,v in dic['headers'].iteritems())
         return '&'.join('%s=%s' % (k,str(v).encode('hex')) for k,v in dic.iteritems())
 
     def load_data(self, qs):
-        dic = dict((k,v.decode('hex')) for k,v in (x.split('=') for x in qs.split('&')))
-        if 'headers' in dic:
-            dic['headers'] = dict((k,v.decode('hex')) for k,v in (x.split('=') for x in dic['headers'].split('&')))
-        return dic
+        return dict((k,v.decode('hex')) for k,v in (x.split('=') for x in qs.split('&')))
+
+    def _process_request(self, req):
+        data = req.read_body()
+        rawrange, range = self._process_range(req.headers)
+        if req.command=='GET' and self.add_range(req.url, req.headers):
+            req.headers['Range'] = range
+        request = {'method':req.command, 'url':req.url.geturl(), 'body':data,
+                   'headers':req.headers, 'range':range}
+        return request, rawrange
 
 
 init_time = 2
